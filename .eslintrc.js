a394e9128ef874f956070008ec0a0dee30f063e7
Ilya_Karachun <Karachun_ilya94@mail.ru>
2023-03-13 15:24:26 +0300

    feat: add compose

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    env: {
+        node: true,
+    },
+    extends: [
+        'airbnb-base',
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+};
--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-const http = require('http')
-const port = 2000
+import http from 'node:http';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const PORT = process.env.APP_PORT || 2000;
 
 http.createServer(function (req, res){
     res.setHeader('Content-type', 'text/html;charset = utf-8;')
     if (req.url === '/') {
-        // res.statusCode(200)
         res.write('<h1>Hi, Mikhail !!! Here will be my project about hospital</h1>')
     } else {
-        // res.statusCode(404)
         res.write('<h1>You can\'t found pages here(</h1>')
     }
     res.end()
-}).listen(port)
+}).listen(PORT)
 
 
